c1823f0aea2e1321b72f849f7d3735aa73d99b87
Paul Kamau <engpaulkamau@gmail.com>
2023-08-31 00:30:35 -0700

    Pycodestyle change in 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -5,9 +5,12 @@ The class is empty and returns nothing.
 '''
 class Square:
     '''This class defines a square object
+
     The class is empty
     No modules are imported in this class
-    AArgs:
+   
+    Args:
         There are no argumnents in this class
     '''
+
     pass
