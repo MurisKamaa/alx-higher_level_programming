99e2150255f49af71673a60780255a66ad5684cd
Paul Kamau <engpaulkamau@gmail.com>
2023-08-31 00:20:02 -0700

    Added documentation in 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    '''This class defines a square object
+    The class is empty
+    No modules are imported in this class
+    AArgs:
+        There are no argumnents in this class
+    '''
     pass
