7ea175538fef51c09dd59410597a3a32f9324a6e
Paul Kamau <engpaulkamau@gmail.com>
2023-08-31 00:34:00 -0700

    Removed white space in 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,12 +3,14 @@
 '''This module coontains an empty class
 The class is empty and returns nothing.
 '''
+
+
 class Square:
     '''This class defines a square object
 
     The class is empty
     No modules are imported in this class
-   
+
     Args:
         There are no argumnents in this class
     '''
