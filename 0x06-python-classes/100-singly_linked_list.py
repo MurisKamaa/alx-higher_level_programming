81d3a2eebfb4bb2cc6f11ffdc86984366a6018ea
Paul Kamau <engpaulkamau@gmail.com>
2023-08-31 02:35:29 -0700

    Added documentations

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,34 +6,51 @@ a node of a singly linked list
 
 class Node:
     """A class that defines a node of a singly linked list
+    The class has various methods as explained in the descriptions
     """
+
     def __init__(self, data, next_node=None):
+        """Method to initialize the Node object
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """ Method to return the data value
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """ Method to set the data value of the Node object
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """Method to return the next node
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """Method to set the value of the next node
+        """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """Defines a class named SinglyLinkedList
+    The class has various methods
+    """
     def __str__(self):
+        """Method to initialize a string of the SinglyLinkedList method
+        """
         rtn = ""
         ptr = self.__head
 
@@ -46,9 +63,13 @@ class SinglyLinkedList:
         return rtn
 
     def __init__(self):
+        """ Method to initialize the SinglyLinkedList object
+        """
         self.__head = None
 
     def sorted_insert(self, value):
+        """Method to sort and insert the new node
+        """
         ptr = self.__head
 
         while ptr is not None:
