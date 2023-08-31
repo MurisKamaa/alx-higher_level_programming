e320e61614ce3c456dc48b4f4b57d15188628063
Paul Kamau <engpaulkamau@gmail.com>
2023-08-31 00:26:48 -0700

    Added documentation to 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''This module coontains an empty class
+The class is empty and returns nothing.
+'''
 class Square:
     '''This class defines a square object
     The class is empty
