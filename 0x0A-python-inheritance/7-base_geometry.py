3ececb11c4b1e153dfefa088adc7f11a74f690c1
MurisKamaa <engpaulkamau@gmail.com>
2023-11-06 07:59:33 -0800

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module has 3 classes: One defines attributes of shapes,
+Second defines the are of the shape and the third receives
+the value property
+"""
+
+
+class BaseGeometry:
+    """ Class that defines the attributes of Geometric Shapes """
+
+    def area(self):
+        """ Method that defines the area of a geomtric shape """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Method that recieves the value property
+
+        Árgs:
+            name: name of the object
+            value: value of the property
+
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
